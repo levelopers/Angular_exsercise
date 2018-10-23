49bf6ac0d9433e1eb79dce54df4c4b766a29f06c
levelopers <38830527+levelopers@users.noreply.github.com>
2018-10-22 21:17:34 -0400

    install angular
    18/10/22

---
+++ helloworld/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component'; //new Component
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent//ng g c test --> test component added as module
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ helloworld/src/app/test/test.component.ts
@@ -0,0 +1,121 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-test',//attribute  //'.app-test' class selector //'app-test' html tag selector
+  template:`
+  <div >
+    <h2>h2 is {{varH2}}</h2>
+    <p>{{getHref()}}</p>
+    <h2 class = "medium">class binding</h2>
+    <h2 [class.success] ="true">class binding</h2>
+    <h2 [ngClass] ="messageClasses">class binding</h2>
+    <h2 [style.color] = "isError? 'red' : 'green'">style binding</h2>
+    <h2 [style.color] = "highlight">class binding</h2>
+    <h2 [ngStyle] ="styleBinding">class binding</h2>
+    <input (click) = "handleClick($event)"type= "submit" value = "sss">
+    {{greeting}}
+    <br>
+    <input #myInput type = "text">
+    <input type = "button" (click) = "logInput(myInput.value)">
+    <p>template reference</p>
+    <br>
+    <input type = "text" [(ngModel)] = 'modleVar'>
+    {{modleVar}}
+
+    <div *ngIf = "renderFirst; then thenBlock;else elseBlock"></div>
+    <ng-template #thenBlock>
+    <h3>then block</h3>
+    </ng-template >
+
+    <ng-template #elseBlock>
+    <h3>else block</h3>
+    </ng-template >
+
+    <div *ngFor = "let x of array;index as i;even as e">
+    <p [style.color]="x">{{e}}{{i}}{{x}}</p>
+    </div>
+
+    <h3>{{"hello" + parentData}}</h3>
+
+
+
+
+
+
+
+    <p>
+      test works!
+      this is in test component
+
+    </p>
+
+  </div>
+  ` ,
+  styles: [`
+    .success{
+      color:green
+    }
+    .medium{
+      color:yellow
+    }
+    .special{
+      font-style:italic
+    }
+    .fail{
+      color:red
+    }
+    `]
+})
+export class TestComponent implements OnInit {
+
+  public varH2 = "h2 variable name" //variable format
+  public isSuccess = true
+  public isSpecial = true
+  public isError = false
+  public highlight = "orange"
+  public messageClasses = { //combine classes as object pass into ng.class attribute
+    "success" : this.isSuccess,
+    "fail" : !this.isSuccess,
+    "special": this.isSpecial
+  }
+  public styleBinding = {
+    color : "blue",
+    fontStyle:"italic"
+  }
+
+  public greeting = ""
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+  }
+  getHref(){ //function format //html like println in java
+    this.href = window.location.href
+    return this.href
+  }
+  handleClick(e){
+    console.log(e)
+
+    // setInterval(()=>{this.greeting += "welcome!!"},30)
+    this.greeting += "welcome!!"
+
+  }
+  logInput(input){
+    console.log(input)
+  }
+
+  public modleVar = ""
+
+  public renderFirst = false
+
+  public array = ["red","yellow","orange","blue","green"]
+
+  @Input() public parentData;
+
+
+
+}
